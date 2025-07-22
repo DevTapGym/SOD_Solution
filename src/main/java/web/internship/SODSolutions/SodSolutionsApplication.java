7a1103350f5e63ec905b6a5b96f9a4e54c7b52aa
DevTapGym <2001224389@huit.edu.vn>
2025-07-22 08:28:49 +0700

    First commit

---
+++ src/main/java/web/internship/SODSolutions/SodSolutionsApplication.java
@@ -0,0 +1,13 @@
+package web.internship.SODSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/internship/SODSolutions/SodSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package web.internship.SODSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
